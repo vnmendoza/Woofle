db707657912145467afe0dff740effcb3db50626
Phuoc Le <phuocle3210@gmail.com>
2021-12-10 16:03:29 -0800

    added name display for swipe cards

--- lib/root/explorer_page.dart
+++ lib/root/explorer_page.dart
@@ -76,48 +76,63 @@ class _TinderState extends State<Tinder> {
   List<Card> cards = [
     Card(
       data[0]['linki'],
+      data[0]['name'],
     ),
     Card(
       data[1]['linki'],
+      data[1]['name'],
     ),
     Card(
       data[2]['linki'],
+      data[2]['name'],
     ),
     Card(
       data[3]['linki'],
+      data[3]['name'],
     ),
     Card(
       data[4]['linki'],
+      data[4]['name'],
     ),
     Card(
       data[5]['linki'],
+      data[5]['name'],
     ),
     Card(
       data[6]['linki'],
+      data[6]['name'],
     ),
     Card(
       data[7]['linki'],
+      data[7]['name'],
     ),
     Card(
       data[8]['linki'],
+      data[8]['name'],
     ),
     Card(
       data[9]['linki'],
+      data[9]['name'],
     ),
     Card(
       data[10]['linki'],
+      data[10]['name'],
     ),
     Card(
       data[11]['linki'],
+      data[11]['name'],
     ),
     Card(
       data[12]['linki'],
+      data[12]['name'],
     ),
     Card(
       data[13]['linki'],
+      data[13]['name'],
     ),
     Card(
       data[14]['linki'],
+      data[14]['name'],
     )
   ];
 
@@ -139,7 +154,8 @@ class Card extends StatelessWidget {
   // Made to distinguish cards
   // Add your own applicable data here
   String linki = '';
-  Card(this.linki);
+  var name;
+  Card(this.linki, this.name);
 
   @override
   Widget build(BuildContext context) {
@@ -151,10 +167,16 @@ class Card extends StatelessWidget {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16.0),
           ),
-          child: Image.asset(linki,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              fit: BoxFit.cover),
+          child: Stack(
+            children: [
+              Image.asset(linki,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  fit: BoxFit.cover),
+              Text('$name',
+                  style: TextStyle(color: Colors.white, fontSize: 60)),
+            ],
+          ),
         ),
       ),
       // onSwipeRight, left, up, down, cancel, etc...
